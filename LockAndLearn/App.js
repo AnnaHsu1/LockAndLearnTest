3184fa0c179657b1a2cb336c00cc9235f8fbc689
AnnaHsu1 <annahsu2000@hotmail.com>
2023-09-23 11:32:02 -0400

    Test commit

--- LockAndLearn/App.js
+++ LockAndLearn/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is the beginning of Lock & Learn!</Text>
+      <Text>Test commit</Text>
       <StatusBar style="auto" />
     </View>
   );
