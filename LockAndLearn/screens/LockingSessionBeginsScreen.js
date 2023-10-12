aafabdb50d8ca1c7217dd0bd7b7a3542bd14ec6d
LeonZhang7 <zhang.leon07@gmail.com>
2023-10-11 23:25:23 -0400

    Creation of LockingSessionBeginsScreen.js
    Creation of LockingSessionBeginsScreen.js, initial commit with imports
    
    Co-Authored-By: Fatema Akther <78175202+fatema-a@users.noreply.github.com>

---
+++ LockAndLearn/screens/LockingSessionBeginsScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  navigation,
+} from "react-native";
+
+const LockingSessionBeingsScreen = ({ navigation }) => {
+
+    // return(
+
+    // );
+};
