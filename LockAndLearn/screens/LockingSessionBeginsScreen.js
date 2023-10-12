75224a44a495fd53aecadf9453ece30f172c72d9
fatema-a <fatema_n_a@hotmail.com>
2023-10-11 23:52:42 -0400

    adding a pop up for ending a session
    now users can end session by the pop up or stay in current screen
    
    Co-Authored-By: LeonZhang7 <78509837+LeonZhang7@users.noreply.github.com>

--- LockAndLearn/screens/LockingSessionBeginsScreen.js
+++ LockAndLearn/screens/LockingSessionBeginsScreen.js
@@ -1,21 +1,45 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  navigation,
-} from "react-native";
+import { StyleSheet, Text, TextInput, View, Button, Modal } from "react-native";
 
-const LockingSessionBeingsScreen = ({ navigation }) => {
+const LockingSessionBeginsScreen = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const closeSession = () => {
+    if (password === "your_password") { // Replace "your_password" with the actual password
+      navigation.goBack();
+    } else {
+      // Handle incorrect password
+    }
+    setModalVisible(false);
+  };
+
   return (
     <View style={{ flex: 1, backgroundColor: "#407BFF" }}>
       <Text>Locking Session Begins</Text>
-      <Button title="End Session" onPress={() => navigation.goBack()} />
+      <Button title="End Session" onPress={() => setModalVisible(true)} />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <View style={{ backgroundColor: "#fff", padding: 20 }}>
+            <Text>Enter Password:</Text>
+            <TextInput
+              secureTextEntry={true}
+              value={password}
+              onChangeText={text => setPassword(text)}
+            />
+            <Button title="Enter" onPress={closeSession} />
+            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
 
-export default LockingSessionBeingsScreen;
+export default LockingSessionBeginsScreen;
+
