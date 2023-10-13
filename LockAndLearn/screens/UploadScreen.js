3c46a56001a480bdf020ae03cb55abf12b4fb4bf
Aykchen <aliceykchen01@gmail.com>
2023-10-13 02:36:39 -0400

    Update UI of upload screen

--- LockAndLearn/screens/UploadScreen.js
+++ LockAndLearn/screens/UploadScreen.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Image, Platform } from "react-native";
+import { StyleSheet, Text, View, Button, Image, Platform, ImageBackground } from "react-native";
 import { React, useState } from "react";
 import * as DocumentPicker from "expo-document-picker";
 import { TouchableOpacity } from "react-native";
@@ -7,6 +7,7 @@ import { TouchableOpacity } from "react-native";
 const UploadScreen = () => {
   const [fileName, setFileName] = useState();
 
+  // receive file uploaded by user and (to be redirect to server)
   const fileSelectedHandler = async () => {
     let result = await DocumentPicker.getDocumentAsync({});
     if (Platform.OS === "web") {
@@ -17,19 +18,34 @@ const UploadScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.selectFiles}>Select files</Text>
-      <View style={styles.uploadContainer}>
-        <Text style={styles.supportedFormats}>Supported formats:</Text>
-        <Text style={styles.supportedFormats}>PDF, TXT, DOCX</Text>
-        <TouchableOpacity testID="selectButton" title="Select File" onPress={fileSelectedHandler} accept=".pdf, docx, .txt">
-          <Image source={require("../assets/uploadDashedZone.png")} style={styles.image} />
-        </TouchableOpacity>
-      </View>
-      <Text>{fileName}</Text>
-      <Button testID="uploadButton" title="Upload" />
-      <StatusBar style="auto" />
-    </View>
+    // display the blue background on top of screen
+    <ImageBackground
+      source={require("../assets/backgroundCloudyBlobsFull.png")}
+      resizeMode="cover"
+      style={styles.container}  
+      >
+      {/* display container to select file with the appropriate formats */}
+      <View style={styles.containerFile}>
+        <Text style={styles.selectFiles}>Select files</Text>
+        <View style={{flex:1, alignItems: "center", justifyContent: "space-evenly",}}>
+          {/* display button to upload file */}
+          <TouchableOpacity testID="selectButton" onPress={fileSelectedHandler} accept=".pdf, docx, .txt">
+            <ImageBackground           
+              style={styles.imageUpload} 
+              source={require("../assets/uploadDashedZone.png")}>
+              <Text style={styles.supportedFormats}>Supported formats:{"\n"}PDF, TXT, DOCX</Text>
+            </ImageBackground>
+          </TouchableOpacity>
+          <TouchableOpacity
+          style={styles.buttonUpload}
+          testID="uploadButton">
+          <Text style={styles.buttonText}>Upload</Text>
+          </TouchableOpacity>
+          {/* display uploaded file */}
+          <Text style={styles.filesText}>{fileName}</Text>
+        </View>
+      </View> 
+    </ImageBackground>
   );
 };
 
@@ -38,29 +54,55 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  },
-  uploadContainer: {
-    alignItems: "center",
     justifyContent: "center",
-    textAlign: "center"
+    width:"100%", 
+    height:"100%"
   },
-  inputContainer: {
-    alignItems: "center"
+  containerFile: {
+    flex:1,
+    backgroundColor: "#FAFAFA",
+    alignItems: "center",
+    width: "100%",
+    borderTopLeftRadius:40,
+    borderTopRightRadius:40,
+    marginTop:"5%"
   },
   selectFiles: {
     color: "#696969",
     fontSize: 36,
-    fontWeight: "500"
-  },
-  image: {
-    zIndex: 1
+    fontWeight: "500",
+    marginTop:"1%"
   },
   supportedFormats: {
     color: "#ADADAD",
     fontSize: 15,
-    fontWeight: "600"
-  }
+    fontWeight: "600",
+    textAlign: "center",
+    marginTop:"95%"
+  },
+  imageUpload:{
+    resizeMode: "contain",
+    width: 198,
+    height: 250,
+  },
+  filesText:{
+    padding: 10,
+  },
+  buttonUpload:{
+    backgroundColor: "#407BFF",
+    width: 190,
+    height: 45,
+    borderRadius: 9,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  buttonText:{
+    color: "#FFFFFF", 
+    alignItems:"center", 
+    fontSize: 20, 
+    fontWeight: "500"
+  },
 });
 
 export default UploadScreen;
