fd50b6741a9401884b60c498965cc1bb34a08ba2
LeonZhang7 <zhang.leon07@gmail.com>
2023-10-12 22:03:25 -0400

    Styled Modal Password
    - Fixed spacing of buttons in pop-up
    - When wrong password is entered, the border of password input box becomes red
    - added blue border to the input box
    
    Co-Authored-By: Fatema Akther <78175202+fatema-a@users.noreply.github.com>

--- LockAndLearn/screens/LockingSessionBeginsScreen.js
+++ LockAndLearn/screens/LockingSessionBeginsScreen.js
@@ -12,6 +12,7 @@ const LockingSessionBeginsScreen = () => {
   const navigation = useNavigation(); // Get the navigation object
   const [modalVisible, setModalVisible] = useState(false);
   const [password, setPassword] = useState("");
+  const [passwordBorderColor, setPasswordBorderColor] = useState("#407BFF"); // Initialize border color
 
   const closeSession = () => {
     if (password === "1234") {
@@ -19,13 +20,12 @@ const LockingSessionBeginsScreen = () => {
       navigation.navigate("Home");
     } else {
       // Handle incorrect password
-      setModalVisible(false);
+      setPasswordBorderColor("red"); // Change border color to red
     }
   };
 
   return (
     <View style={styles.container}>
-      <Spacer height={20} />
       <View style={styles.centeredContent}>
         <Text style={styles.logoText}>It's time to</Text>
         <Spacer height={20} />
@@ -36,14 +36,12 @@ const LockingSessionBeginsScreen = () => {
           style={styles.logo}
         />
       </View>
-      <Spacer height={20} />
       <TouchableOpacity
         style={styles.customButton}
         onPress={() => setModalVisible(true)}
       >
         <Text style={styles.customButtonText}>End Session</Text>
       </TouchableOpacity>
-      <Spacer height={20} />
       <Modal
         animationType="slide"
         transparent={true}
@@ -53,25 +51,27 @@ const LockingSessionBeginsScreen = () => {
           <View style={styles.modalContent}>
             <Text style={styles.modalText}>Enter Password:</Text>
             <TextInput
-              style={styles.modalInput}
+              style={[styles.modalInput, { borderColor: passwordBorderColor }]} // Apply border color dynamically
               secureTextEntry={true}
               value={password}
-              onChangeText={(text) => setPassword(text)}
+              onChangeText={(text) => {
+                setPassword(text);
+                setPasswordBorderColor("#407BFF"); // Reset border color when typing
+              }}
             />
-            <Spacer height={20} />
             <View style={styles.modalButtons}>
               <TouchableOpacity
-                style={styles.customButton}
+                style={styles.modalButton}
                 onPress={closeSession}
               >
-                <Text style={styles.customButtonText}>Enter</Text>
+                <Text style={styles.modalButtonText}>Enter</Text>
               </TouchableOpacity>
               <Spacer width={10} />
               <TouchableOpacity
-                style={styles.customButton}
+                style={styles.modalButton}
                 onPress={() => setModalVisible(false)}
               >
-                <Text style={styles.customButtonText}>Cancel</Text>
+                <Text style={styles.modalButtonText}>Cancel</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -95,6 +95,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF", // Background color
     padding: 10,
     borderRadius: 5,
+    position: "absolute", // Position the button
+    top: 20, // Position from the top
+    right: 20, // Position from the right
   },
   customButtonText: {
     color: "#3E5CAA", // Text color
@@ -110,27 +113,46 @@ const styles = StyleSheet.create({
   modalContent: {
     backgroundColor: "#fff",
     padding: 30, // Increase the padding for a bigger modal
+    width: 80 + '%', // Set the width to 80% of the screen
+    height: 40 + '%', // Set the height to 40% of the screen
     borderRadius: 10, // Rounded corners
+    justifyContent: "center", // Center content vertically
   },
   modalText: {
     marginBottom: 10, // Add spacing below the text
   },
   modalInput: {
     marginBottom: 10, // Add spacing below the input field
+    borderWidth: 1, // Add a border
+    borderColor: "#407BFF", // Initial border color
+    borderRadius: 5,
+    padding: 10,
   },
   modalButtons: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  logo: {},
+  modalButton: {
+    backgroundColor: "#407BFF",
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+  },
+  modalButtonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  logo: {
+    marginTop: "25%", // Position the image 25% from the top
+  },
   logoText: {
     color: "#FFFFFF",
     fontSize: 24, // Larger font size
-    marginTop: 20, // Add some space above the text
   },
   logoTextBig: {
     color: "#FFFFFF",
-    fontSize: 36, // Larger font size
+    fontSize: 36, // Larger font size for LOCK & LEARN !
   },
 });
 
