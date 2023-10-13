3bfd75404016735564745f3edf58174c2bde9642
William Chong <80657298+Chovv@users.noreply.github.com>
2023-10-13 00:48:44 -0400

    New users can be added to database via signup
    Implemented logic to add users who sign up into the database.

--- LockAndLearn/screens/SignupScreen.js
+++ LockAndLearn/screens/SignupScreen.js
@@ -13,114 +13,152 @@ import { RadioButton } from 'react-native-paper';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const SignupScreen = ({ navigation }) => {
-    const [fdata, setFdata] = useState({
-        FirstName: '',
-        LastName: '',
-        Account: '',
-        Email: '',
-        Password: '',
-        CPassword:'',
-        DOB: '',
-    })
-    const [errormsg, setErrormsg] = useState(null);
-    const [text, setText] = useState("");
-    const [checked, setChecked] = React.useState('first');
-    const SendToBackend = () => {
-        console.log(fdata);
-        if (fdata.FirstName == '' ||
-            fdata.LastName == '' ||
-            fdata.Email == '' ||
-            fdata.Password == '' ||
-            fdata.CPassword == '' ||
-            fdata.DOB == '') {
-            setErrormsg('All fields are required');
-            return;
-        }
-        else {
-            if (fdata.Password != fdata.CPassword) {
-                setErrormsg('The passwords must be the same');
-                return;
-            }
-        }
+
+  const [fdata, setFdata] = useState({
+    FirstName: "",
+    LastName: "",
+    Account: "",
+    Email: "",
+    Password: "",
+    CPassword: "",
+    DOB: "",
+  });
+
+  const [errormsg, setErrormsg] = useState(null);
+  const [text, setText] = useState("");
+  const [checked, setChecked] = React.useState("first");
+
+  const handleSubmit = async () => {
+    console.log(fdata);
+  
+    if (
+      fdata.FirstName === "" ||
+      fdata.LastName === "" ||
+      fdata.Email === "" ||
+      fdata.Password === "" ||
+      fdata.CPassword === "" ||
+      fdata.DOB === ""
+    ) {
+      setErrormsg("All fields are required");
+      return;
+    } else {
+      if (fdata.Password !== fdata.CPassword) {
+        setErrormsg("The passwords must be the same");
+        return;
+      }
     }
+
+    // Package the user data into a JSON format and ship it to the backend
+    try {
+      const response = await fetch('http://localhost:4000/users/signup', {  
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fdata), // Send user data as JSON
+      });
+
+      const data = await response.json();
+
+      if (response.status === 201) {
+        // User created successfully
+        console.log("User created successfully in database!", data);
+      } else {
+        
+        // Store the error message in state
+        setErrormsg(data.message); 
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+
+  };
+  
   return (
     <View style={styles.container}>
-      {
-              errormsg ? <Text style={{ color: 'red' }}>{errormsg}</Text>:null
-          }
+      {errormsg ? <Text style={{ color: "red" }}>{errormsg}</Text> : null}
       <Text style={styles.title}>Create your account</Text>
       <Text style={styles.field}>Email</Text>
       <TextInput
         style={styles.textbox}
-              onChangeText={(newText) => setFdata({...fdata, Email: newText})}
+        onChangeText={(newText) => setFdata({ ...fdata, Email: newText })}
         defaultValue={text}
       />
-          
-          <View style={styles.row}>
-              <View style={styles.nameInputContainer}>
-                  <Text style={styles.field}>First Name</Text>
-                  <TextInput
-                      style={styles.textbox}
-                      onChangeText={(newText) => setFdata({ ...fdata, FirstName: newText })}
-                      defaultValue={text}
-                  />
-              </View>
 
-              <View style={styles.nameInputContainer}>
-                  <Text style={styles.field}>Last Name</Text>
-                  <TextInput
-                      style={styles.textbox}
-                      onChangeText={(newText) => setFdata({ ...fdata, LastName: newText })}
-                      defaultValue={text}
-                  />
-              </View>
-          </View>
-      
-
-          <Text style={styles.field}>Select your account type</Text> 
-          <View>
-              <RadioButton
-                  value="parent"
-                  status={checked === 'parent' ? 'checked' : 'unchecked'}
-                  onPress={() => setChecked('parent')}
-              /> <Text>Parent</Text>
-              <RadioButton
-                  value="teacher"
-                  status={checked === 'teacher' ? 'checked' : 'unchecked'}
-                  onPress={() => setChecked('teacher')}
-              /> <Text>Teacher</Text>
-          </View>
-      <Text style={styles.field}>Birth Date</Text>
+      <View style={styles.row}>
+        <View style={styles.nameInputContainer}>
+          <Text style={styles.field}>First Name</Text>
           <TextInput
-              style={styles.textbox}
-              onChangeText={(newText) => setFdata({ ...fdata, DOB: newText })}
-              defaultValue={text}
+            style={styles.textbox}
+            onChangeText={(newText) =>
+              setFdata({ ...fdata, FirstName: newText })
+            }
+            defaultValue={text}
           />
+        </View>
+
+        <View style={styles.nameInputContainer}>
+          <Text style={styles.field}>Last Name</Text>
+          <TextInput
+            style={styles.textbox}
+            onChangeText={(newText) =>
+              setFdata({ ...fdata, LastName: newText })
+            }
+            defaultValue={text}
+          />
+        </View>
+      </View>
+
+      <Text style={styles.field}>Select your account type</Text>
+      <View>
+        <RadioButton
+          value="parent"
+          status={checked === "parent" ? "checked" : "unchecked"}
+          onPress={() => setChecked("parent")}
+        />{" "}
+        <Text>Parent</Text>
+        <RadioButton
+          value="teacher"
+          status={checked === "teacher" ? "checked" : "unchecked"}
+          onPress={() => setChecked("teacher")}
+        />{" "}
+        <Text>Teacher</Text>
+      </View>
+      <Text style={styles.field}>Birth Date</Text>
+      <TextInput
+        style={styles.textbox}
+        onChangeText={(newText) => setFdata({ ...fdata, DOB: newText })}
+        defaultValue={text}
+      />
       <Text style={styles.field}>Password</Text>
       <TextInput
-              style={styles.textbox}
-              secureTextEntry={true }
-              onChangeText={(newText) => setFdata({ ...fdata, Password: newText })}
+        style={styles.textbox}
+        secureTextEntry={true}
+        onChangeText={(newText) => setFdata({ ...fdata, Password: newText })}
         defaultValue={text}
       />
 
       <Text style={styles.field}>Confirm password</Text>
       <TextInput
-              style={styles.textbox}
-              secureTextEntry={true}
-              onChangeText={(newText) => setFdata({ ...fdata, CPassword: newText })}
+        style={styles.textbox}
+        secureTextEntry={true}
+        onChangeText={(newText) => setFdata({ ...fdata, CPassword: newText })}
         defaultValue={text}
-          />
-          <Button color="#4F85FF"
-              onPress = {() => { SendToBackend(); }}
-              title="Sign up" />
+      />
+      <Button
+        color="#4F85FF"
+        onPress={() => {
+          handleSubmit();
+        }}
+        title="Sign up"
+      />
 
-          <Text style={styles.link}>Already have an account? &nbsp;
-              <Text styles={styles.link}
-              onPress={() => navigation.navigate("Login")}>
-                  Sign in
-              </Text>
-          </Text>
+      <Text style={styles.link}>
+        Already have an account? &nbsp;
+        <Text styles={styles.link} onPress={() => navigation.navigate("Login")}>
+          Sign in
+        </Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
