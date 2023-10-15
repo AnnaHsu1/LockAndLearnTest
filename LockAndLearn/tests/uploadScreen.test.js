5edb8c6502813866362df5ab1447823bc4b19ae8
Aykchen <aliceykchen01@gmail.com>
2023-10-15 01:50:04 -0400

    Add tests for connection to server

--- LockAndLearn/tests/uploadScreen.test.js
+++ LockAndLearn/tests/uploadScreen.test.js
@@ -3,6 +3,9 @@ import React from "react";
 import UploadScreen from "../screens/UploadScreen";
 import { render, fireEvent, waitFor } from "@testing-library/react-native";
 import * as DocumentPicker from "expo-document-picker";
+const fetchMock = require('jest-fetch-mock');
+fetchMock.enableMocks();
+const FormData = require('form-data');
 
 jest.mock("expo-document-picker", () => ({
   getDocumentAsync: jest.fn().mockReturnValue({
@@ -14,6 +17,50 @@ jest.mock("expo-document-picker", () => ({
     ]
   })
 }));
+
+// Using uploadFilesHandler function from EditUploadScreen 
+const uploadFilesHandler = async (fileData) => {
+  await fetch("http://localhost:4000/files/uploadFiles", {
+    method: "POST",
+    body: fileData
+  })
+};
+
+describe('uploadFilesHandler', () => {
+  it('make POST request and test connection to server', async () => {
+    // Mock response
+    fetchMock.mockResponseOnce(JSON.stringify({ message: 'File uploaded' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+    
+    // Mock test file
+    const fs = require('fs'); 
+    const fileStream = fs.createReadStream('example.txt');
+    const formData = new FormData();
+    formData.append('files', fileStream, 'example.txt');
+    
+    // Check fetch is called with its expected URL, method and body
+    await uploadFilesHandler(formData);
+    expect(fetchMock).toHaveBeenCalledWith('http://localhost:4000/files/uploadFiles', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // Check response message afect sending to server
+    const response = await fetch("http://localhost:4000/files/uploadFiles", {
+    method: "POST",
+    body: formData
+    });
+    if (response.status) {
+      expect(response.status).toBe(200);
+      if (response.headers.get('content-type')?.includes('application/json')) {
+        const data = await response.json();
+        expect(data.message).toBe('File uploaded');
+      };
+    }; 
+  });
+});
 
 describe("upload file tests", () => {
   test("select button is working as expected by opening file picker", async () => {
