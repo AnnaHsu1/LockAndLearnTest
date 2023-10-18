be0ccf41cfe67605bd8336d121d85854bd32300c
fatema-a <fatema_n_a@hotmail.com>
2023-10-18 00:05:25 -0400

    adding end session button and fixing styling

--- LockAndLearn/screens/LockingSchedulePresentation.js
+++ LockAndLearn/screens/LockingSchedulePresentation.js
@@ -1,74 +1,195 @@
-// LockingSchedulePresentation.js
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal, TextInput } from 'react-native';
 
-import React from 'react';
+const LockingSchedulePresentation = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [password, setPassword] = useState('');
+  const [passwordBorderColor, setPasswordBorderColor] = useState('#407BFF');
 
-function LockingSchedulePresentation() {
-  const styles = {
-    lessonContainer: {
-      fontFamily: 'Arial, sans-serif',
-      width: '100vw',
-      height: '100vh',
-      borderRadius: '15px',
-      backgroundColor: '#407BFF',
-      display: 'flex',
-      flexDirection: 'column',
-      padding: '20px'
-    },
-    lessonHeader: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginBottom: '20px'
-    },
-    endSessionButton: {
-      backgroundColor: '#fff',
-      border: 'none',
-      padding: '5px 10px',
-      borderRadius: '5px',
-      cursor: 'pointer'
-    },
-    lessonContent: {
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    },
-    startLearningButton: {
-      backgroundColor: '#fff',
-      border: 'none',
-      padding: '10px 20px',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      margin: '20px 0'
-    },
-    learningImage: {
-      width: '80%',
-      height: 'auto'
+  const closeSession = () => {
+    if (password === '1234') {
+      // Handle session end logic here
+      setModalVisible(false);
+    } else {
+      setPasswordBorderColor('red');
     }
   };
 
   return (
-    <div style={styles.lessonContainer}>
-      <div style={styles.lessonHeader}>
-        <span>9:41</span>
-        <button style={styles.endSessionButton}>End Session</button>
-      </div>
-      <div style={styles.lessonContent}>
-        <h1>Lesson Schedule</h1>
-        <h2>Math</h2>
-        <h3>⏱️ 1h40</h3>
-        <ul>
-          <li>Additions</li>
-          <li>Subtractions</li>
-          <li>Quiz (30 questions)</li>
-        </ul>
-        <p>The quiz must be passed with a grade of 75% or higher</p>
-        <button style={styles.startLearningButton}>Start Learning</button>
-        <img src="path_to_your_image_here" alt="Learning Illustration" style={styles.learningImage} />
-      </div>
-    </div>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>9:41</Text>
+        <TouchableOpacity style={styles.endSessionButton} onPress={() => setModalVisible(true)}>
+          <Text style={styles.endSessionText}>End Session</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.lessonContent}>
+        <Text style={styles.lessonTitle}>Lesson Schedule</Text>
+        <Text style={styles.subject}>Math</Text>
+        <Text style={styles.duration}>⏱️ 1h40</Text>
+        <Text style={styles.details}>• Additions</Text>
+        <Text style={styles.details}>• Subtractions</Text>
+        <Text style={styles.details}>• Quiz (30 questions)</Text>
+        <Text style={styles.passCriteria}>
+          The quiz must be passed with a grade of 75% or higher
+        </Text>
+        <TouchableOpacity style={styles.startLearningButton}>
+          <Text style={styles.startLearningText}>Start Learning</Text>
+        </TouchableOpacity>
+        {/* <Image
+          source={require('../assets/learning_illustration.png')}
+          style={styles.learningIllustration}
+        /> */}
+      </View>
+
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>End Session</Text>
+            <Text style={styles.modalText}>Enter Password:</Text>
+            <TextInput
+              style={[styles.modalInput, { borderColor: passwordBorderColor }]}
+              secureTextEntry={true}
+              value={password}
+              onChangeText={(text) => {
+                setPassword(text);
+                setPasswordBorderColor('#407BFF');
+              }}
+            />
+            <View style={styles.modalButtons}>
+              <TouchableOpacity style={styles.modalButton} onPress={closeSession}>
+                <Text style={styles.modalButtonText}>Enter</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#407BFF',
+    alignItems: 'center',
+    padding: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 24,
+    color: '#FFFFFF',
+  },
+  endSessionButton: {
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  endSessionText: {
+    color: '#3E5CAA',
+  },
+  lessonContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lessonTitle: {
+    fontSize: 36,
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+  subject: {
+    fontSize: 28,
+    color: '#FFFFFF',
+    marginBottom: 10,
+  },
+  duration: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    marginBottom: 10,
+  },
+  details: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    marginBottom: 5,
+  },
+  passCriteria: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    marginVertical: 20,
+  },
+  startLearningButton: {
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  startLearningText: {
+    color: '#3E5CAA',
+    fontSize: 18,
+  },
+  learningIllustration: {
+    width: 250,
+    height: 250,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 30,
+    width: '80%',
+    height: '40%',
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  modalTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#3E5CAA',
+    marginBottom: 20,
+  },
+  modalText: {
+    marginBottom: 10,
+    color: '#3E5CAA',
+  },
+  modalInput: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#407BFF',
+    borderRadius: 5,
+    padding: 10,
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalButton: {
+    backgroundColor: '#407BFF',
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+  },
+  modalButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
 
 export default LockingSchedulePresentation;
-
