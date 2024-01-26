b4fbb19c88ab294075bd749cbf2838c804a2263d
LeonZhang7 <zhang.leon07@gmail.com>
2024-01-26 00:23:02 -0500

    Added elements needed in schema of reports
    Added elements needed in schema of reports
    
    Co-Authored-By: Fatema Akther <78175202+fatema-a@users.noreply.github.com>

--- LockAndLearn/server/schema/reportSchema.js
+++ LockAndLearn/server/schema/reportSchema.js
@@ -3,10 +3,25 @@ const collectionName = 'Reports';
 
 // Define the schema for the subcategory document
 const reportsSchema = new mongoose.Schema({
-    
-  });
-  
-  // Create the "Subcategory" model using the schema
-  const Reports = mongoose.model('Reports', reportsSchema, collectionName);
-  
-  module.exports = Reports;
+  idOfWp: {
+    type: String,
+    required: true
+  },
+  timeOfReport: {
+    type: String,
+    required: true
+  },
+  idOfReporter: {
+    type: String,
+    required: true
+  },
+  reason: {
+    type: String,
+    required: true
+  }
+});
+
+// Create the "Reports" model using the schema
+const Reports = mongoose.model('Reports', reportsSchema, collectionName);
+
+module.exports = Reports;
