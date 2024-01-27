15e8688295130b7d4dd77240f54a22e6b89e3b23
cotsop@gmail.com <cotsop@gmail.com>
2024-01-27 03:41:29 -0500

    Reviewing answers #22 Completed Logic

--- LockAndLearn/screens/StudyMaterial/QuizGradeScreen.js
+++ LockAndLearn/screens/StudyMaterial/QuizGradeScreen.js
@@ -7,10 +7,72 @@ const QuizGradeScreen = ({ route }) => {
     const quizId = route.params.quizId;
     const grade = route.params.numOfCorrectAnswers;
     const quizLength = route.params.quizLength;
+    const answers = route.params.answers;
+    const questions = route.params.questions;
+    const solutions = route.params.solutions;
+    const [results, setResults] = useState([]);
 
 
     useEffect(() => {
+
+        console.log("ANSWERS FROM GRADE:",answers);
+        console.log("SOLUTIONS FROM GRADE",solutions);
+        console.log("QUESTIONS FROM GRADE",questions);
+
+        let tempResults = [];
+
+        // Loop through the answers and solutions arrays and compare them
+        for (let i = 0; i < quizLength; i++) {
+            // If the answer and solution are both arrays and of equal length
+            if (Array.isArray(answers) && Array.isArray(solutions) 
+            && answers.length === solutions.length){
+            // Compare arrays of strings
+                //if correct answer, populate results with true
+                if (answers[i] === solutions[i]) {
+                    tempResults.push(true);
+                }
+                //if incorrect answer, populate results with false
+                else {
+                    tempResults.push(false);
+                }
+            } else {
+                console.log("Error: answers and solutions are not arrays of equal length");
+            }
+        };
+        //set results to tempResults
+        setResults(tempResults);
     }, []);
+
+    const RenderResults = () => {
+        return (
+            <View style={styles.listQuestions}>
+                <Text style={styles.gradeText}>Here are the results:</Text>
+                <View style={{flexDirection: "row"}}>
+                    <Text style={styles.headerQuestion}>Question</Text>
+                    <Text style={styles.headerAnswer}>Your Answer</Text>
+                    <Text style={styles.headerResult}>Result</Text>
+                </View>
+
+                {results.map((result, index) => {
+                    return (
+                        <View>
+                            <View key={index} style={styles.containerQuestion}>
+                                <Text style={styles.containerQuestionText}>
+                                    {questions[index].questionText}
+                                </Text>
+                                <Text style={styles.containerAnswer}>
+                                    {answers[index]}
+                                </Text>
+                                <Text style={styles.containerAnswer}>
+                                    {solutions[index]}
+                                </Text>
+                            </View>
+                        </View>
+                    );
+                })}
+            </View>
+        );
+    }
 
     return (
         <ImageBackground
@@ -21,6 +83,7 @@ const QuizGradeScreen = ({ route }) => {
             <View style={styles.containerFile}>
                 <Text style={styles.gradeText}>You got {grade} correct answers out of {quizLength}!</Text>
                 <Text style={styles.gradeText}>Your grade is {((grade/quizLength) * 100).toFixed(0)} %</Text>
+                <RenderResults />
             </View>
         </ImageBackground>
     );
@@ -38,7 +101,7 @@ const styles = StyleSheet.create({
     containerFile: {
         flex: 1,
         backgroundColor: '#FAFAFA',
-        alignItems: 'center',
+        alignItems: 'left',
         justifyContent: 'center', // Ensure content is vertically centered
         width: '90%',
         maxWidth: 800,
@@ -54,6 +117,66 @@ const styles = StyleSheet.create({
         fontWeight: 'bold', // Bold font weight
         textAlign: 'center', // Center align the text
         marginBottom: 10, // Space below the text
+    },
+    //Container for the list of questions
+    listQuestions: {
+        flexDirection: 'column',
+    },
+    //Container for each question
+    containerQuestion: {
+        fontSize: 18,
+        color: '#333',
+        fontWeight: 'bold',
+        textAlign: 'left',
+        marginBottom: 10,
+        flexDirection: 'row',
+    },
+    containerQuestionText: {
+        fontSize: 16,
+        color: '#333',
+        textAlign: 'left',
+        marginBottom: 10,
+        marginRight: "39%",
+    },
+    //Container for the given answer
+    containerAnswer: {
+        fontSize: 16,
+        color: '#333',
+        textAlign: 'left',
+        marginBottom: 10,
+        marginRight: "10%",
+    },
+    // Display a green background color and border if correct, red if incorrect
+    containerCorrectAnswer: {
+        backgroundColor: 'green',
+        textAlign: 'center',
+        justifyContent: 'center',
+
+    },
+    containerIncorrectAnswer: {
+        backgroundColor: 'red',
+        textAlign: 'center',
+        justifyContent: 'center',
+    },
+    headerQuestion: {
+        fontSize: 20,
+        color: '#333',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginRight: "50%",
+    },
+    headerAnswer: {
+        fontSize: 20,
+        color: '#333',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginRight: "10%",
+    },
+    headerResult: {
+        fontSize: 20,
+        color: '#333',
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
     
 });
