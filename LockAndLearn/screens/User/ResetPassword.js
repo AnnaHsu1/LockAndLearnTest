684fc7cd9e219d8539e0c87f79a73f8435469031
Ryan Kim <rck2001@hotmail.com>
2024-03-09 17:46:45 -0500

    reset password :white_check_mark:

--- LockAndLearn/screens/User/ResetPassword.js
+++ LockAndLearn/screens/User/ResetPassword.js
@@ -3,10 +3,77 @@ import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-nativ
 import PropTypes from 'prop-types';
 
 const ResetPassword = ({ route, navigation }) => {
+  var bcrypt = require('bcryptjs');
+
+  const userEmail = route.params.email;
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleResetPassword = async () => {
+    if (password === confirmPassword) {
+      //   Send to server
+      console.log('Password reset');
+
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
+
+      try {
+        const response = await fetch(
+          'https://data.mongodb-api.com/app/lock-and-learn-xqnet/endpoint/resetPassword',
+          {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              email: userEmail,
+              password: hash,
+            }),
+          }
+        );
+        const data = await response.json();
+        if (response.status != 200) {
+          console.log(data.msg);
+          setError(data.msg);
+        } else {
+          navigation.navigate('Login');
+        }
+      } catch (error) {
+        console.log(error.msg);
+      }
+    } else {
+      console.log('Passwords do not match');
+    }
+  };
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Text style={styles.title}>Reset Password</Text>
+        <Text style={styles.title}>Reset password for {userEmail}</Text>
+        <View style={[styles.inputContainer, { gap: 10 }]}>
+          <View style={{ width: '100%' }}>
+            <Text style={styles.text}>Please enter your new password</Text>
+            <TextInput
+              style={styles.textInput}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={{ width: '100%' }}>
+            <Text style={styles.text}>Please confirm your new password</Text>
+            <TextInput
+              style={styles.textInput}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <TouchableOpacity onPress={handleResetPassword} style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
